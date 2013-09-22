8b3e8239c38547d93024a837657660058f4b7670
Matthias Krull <m.krull@uninets.eu>
2013-09-22 10:12:40 +0000

    fixed template indentation

--- static/js/App.js
+++ static/js/App.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('about')
-	this.resource('posts')
+    this.resource('about')
+    this.resource('posts')
     this.resource('welcome')
 });
